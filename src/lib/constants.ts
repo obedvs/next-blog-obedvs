943cc7755c6c9be3c16651fd1f0ed1bb9eb2fdeb
obedvs <is.paulvega.17102@gmail.com>
2024-07-30 13:16:56 -0600

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,30 +5,30 @@ export const POSTS: { title: string; href: string; description: string }[] = [
     description:
       "Learn how to build modern web applications using React, a JavaScript library for building user interfaces.",
   },
-  {
-    title: "JavaScript",
-    href: "/blog/javascript",
-    description: "Learn what are new in the JavaScript world",
-  },
+  // {
+  //   title: "JavaScript",
+  //   href: "/blog/javascript",
+  //   description: "Learn what are new in the JavaScript world",
+  // },
   {
     title: "CSS",
     href: "/blog/css",
     description: "Learn how to style your web applications using CSS",
   },
-  {
-    title: "Performance",
-    href: "/blog/performance",
-    description:
-      "Learn how to improve the performance of your web applications",
-  },
-  {
-    title: "Animations",
-    href: "/blog/animations",
-    description: "Learn how to create animations using CSS and JavaScript",
-  },
-  {
-    title: "Career",
-    href: "/blog/career",
-    description: "Learn how to improve your career as a developer",
-  },
+  // {
+  //   title: "Performance",
+  //   href: "/blog/performance",
+  //   description:
+  //     "Learn how to improve the performance of your web applications",
+  // },
+  // {
+  //   title: "Animations",
+  //   href: "/blog/animations",
+  //   description: "Learn how to create animations using CSS and JavaScript",
+  // },
+  // {
+  //   title: "Career",
+  //   href: "/blog/career",
+  //   description: "Learn how to improve your career as a developer",
+  // },
 ];
