c58d79873f235bdae03ef6ba00e42c1e9855d1cb
obedvs <is.paulvega.17102@gmail.com>
2024-09-25 23:24:53 -0600

    Add ArtGora
    And Update dependencies

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,33 +1,12 @@
 export const POSTS: { title: string; href: string; description: string }[] = [
   {
+    title: "Proyectos",
+    href: "/blog/proyectos",
+    description: "Conoce los proyectos que he realizado y cómo los he construido.",
+  },
+  {
     title: "React",
     href: "/blog/react",
     description: "Aprende a construir aplicaciones web modernas utilizando React, una biblioteca de JavaScript para construir interfaces de usuario.",
   },
-  // {
-  //   title: "JavaScript",
-  //   href: "/blog/javascript",
-  //   description: "Learn what are new in the JavaScript world",
-  // },
-  // {
-  //   title: "CSS",
-  //   href: "/blog/css",
-  //   description: "Aprende a darle estilo a tus aplicaciones web utilizando CSS",
-  // },
-  // {
-  //   title: "Performance",
-  //   href: "/blog/performance",
-  //   description:
-  //     "Learn how to improve the performance of your web applications",
-  // },
-  // {
-  //   title: "Animations",
-  //   href: "/blog/animations",
-  //   description: "Learn how to create animations using CSS and JavaScript",
-  // },
-  // {
-  //   title: "Career",
-  //   href: "/blog/career",
-  //   description: "Learn how to improve your career as a developer",
-  // },
 ];
